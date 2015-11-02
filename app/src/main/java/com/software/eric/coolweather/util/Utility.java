cd04344ae0852004757f7536098c942ff5ecc2a0
eric <ericceshi1@163.com>
2015-11-03 00:27:58 +0800

    add drawerLayout, setting action, can change autoUpdateWeatherInfo time.

--- app/src/main/java/com/software/eric/coolweather/util/Utility.java
+++ app/src/main/java/com/software/eric/coolweather/util/Utility.java
@@ -92,6 +92,7 @@ public class Utility {
     }
 
     private static void saveWeatherInfo(Context context, String cityName, String weatherCode, String temp1, String temp2, String weatherDesp, String publishTime) {
+        //TODO: 24小时制
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy年M月d日hh:mm", Locale.CHINA);
         SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
         editor.putBoolean("city_selected", true);
